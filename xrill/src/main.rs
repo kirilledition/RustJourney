03627950bce4a43675d43a12b38055503245743f
Kirill Denisov <kirilledition@protonmail.com>
2025-02-11 05:21:06 +0000

    modularization of xrill

--- xrill/src/main.rs
+++ xrill/src/main.rs
@@ -1,10 +1,8 @@
-mod arguments;
-use tokio::fs;
-use tokio::io::AsyncWriteExt;
+mod programs;
 
 use clap::Parser;
+use clap::Subcommand;
 use std::error::Error;
-use std::path::PathBuf;
 // cargo run fetchpdb 4hhb 8pvw 9cq5 9bwf asf --output downloaded/
 
 #[tokio::main]
@@ -15,48 +13,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .flatten_event(true)
         .init();
 
-    let cli = arguments::Cli::parse();
+    let cli = CLI::parse();
 
     match &cli.command {
-        arguments::Commands::Fetchpdb(arguments) => fetchpdb(arguments).await,
+        Commands::Fetchpdb(arguments) => programs::fetchpdb::run(arguments).await,
     }
 }
 
-async fn fetchpdb(arguments: &arguments::FetchpdbArguments) -> Result<(), Box<dyn Error>> {
-    const BASE_URL: &str = "https://files.rcsb.org/download/";
-    let client = reqwest::Client::new();
-    let base_url = reqwest::Url::parse(BASE_URL)?;
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+pub struct CLI {
+    #[command(subcommand)]
+    pub command: Commands,
+}
 
-    if !arguments.output_path.is_dir() {
-        fs::create_dir_all(arguments.output_path.clone()).await?;
-        tracing::info!("Creating directory {}", arguments.output_path.display());
-    };
-
-    let download_jobs = arguments.codes.iter().map(move |code| {
-        let client = client.clone();
-        let base_url = base_url.clone();
-        let path = arguments.output_path.clone();
-
-        async move {
-            let pdb_filename = PathBuf::from(code).with_extension("pdb");
-            let url = base_url.join(pdb_filename.to_str().unwrap())?;
-
-            let response = client.get(url).send().await?;
-            if !response.status().is_success() {
-                tracing::warn!("Request for {code} was unsuccessfull");
-                return Ok(());
-            }
-
-            let pdb_text = response.bytes().await?;
-            let filename = path.join(pdb_filename);
-
-            tracing::info!("Writing {}", filename.display());
-            let mut file = fs::File::create(filename).await?;
-            file.write_all(&pdb_text).await?;
-            Ok::<(), Box<dyn Error>>(())
-        }
-    });
-
-    futures::future::join_all(download_jobs).await;
-    Ok(())
+#[derive(Subcommand)]
+pub enum Commands {
+    Fetchpdb(programs::fetchpdb::Arguments),
 }
